1a84ff54cd5af1c4120486bf3145559e077ed79e
asqhaqs <xudong_ftd@163.com>
2020-06-29 19:20:13 +0800

    Spring aop

---
+++ java-multithread/src/main/java/com/brianway/learning/java/threadpool/forkjoinpool/Calculator.java
@@ -0,0 +1,11 @@
+package com.brianway.learning.java.threadpool.forkjoinpool;
+
+/**
+ * Create by xudong
+ * Author: xudong
+ * Date: 2020-06-29
+ */
+public interface Calculator {
+
+    long sunUp(long[] numbers);
+}
---
+++ java-multithread/src/main/java/com/brianway/learning/java/threadpool/forkjoinpool/ForkJoinCalculator.java
@@ -0,0 +1,73 @@
+package com.brianway.learning.java.threadpool.forkjoinpool;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
+
+/**
+ * Create by xudong
+ * Author: xudong
+ * Date: 2020-06-29
+ */
+public class ForkJoinCalculator implements Calculator{
+    private ForkJoinPool pool;
+
+
+    private static class SunTask extends RecursiveTask<Long>{
+
+        private long[] numbers;
+        private int from;
+        private int to;
+
+        public SunTask(long[] numbers, int from, int to){
+            this.numbers = numbers;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        protected Long compute(){
+            if(to - from < 6){
+                long total = 0;
+                for(int i = from; i <= to; i++){
+                    total += numbers[i];
+                }
+                return total;
+            }else{
+                int middle = (from + to) / 2;
+                SunTask taskLeft = new SunTask(numbers, from, middle);
+                SunTask taskRight = new SunTask(numbers, middle + 1, to);
+                taskLeft.fork();
+                taskRight.fork();
+                return taskLeft.join() + taskRight.join();
+            }
+
+        }
+
+    }
+
+    public ForkJoinCalculator(){
+        pool = ForkJoinPool.commonPool();
+    }
+
+
+    @Override
+    public long sunUp(long[] numbers) {
+        Long result = pool.invoke(new SunTask(numbers, 0, numbers.length - 1));
+        pool.shutdown();
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+        long result = LongStream.rangeClosed(0, 10000000L).parallel().reduce(0, Long::sum);
+        Instant end = Instant.now();
+        System.out.println("耗时：" + Duration.between(start, end).toMillis() + "ms");
+        System.out.println("结果为：" + result);
+    }
+
+
+}
