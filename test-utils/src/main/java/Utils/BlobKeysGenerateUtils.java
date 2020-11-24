9e36ebd499ea7ee73b50f4856423471f57e2a5a1
xudong03 <xudong03@kuaishou.com>
2020-11-24 22:20:49 +0800

    test

---
+++ test-utils/src/main/java/Utils/BlobKeysGenerateUtils.java
@@ -0,0 +1,13 @@
+package Utils;
+
+import java.util.Calendar;
+
+/**
+ * @author xudong03 <xudong03@kuaishou.com>
+ * Created on 2020-11-04
+ */
+public class BlobKeysGenerateUtils {
+    public static void main(String[] args) {
+        System.out.println(Calendar.getInstance().getTimeInMillis());
+    }
+}
